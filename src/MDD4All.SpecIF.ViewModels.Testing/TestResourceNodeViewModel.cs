81257f065032024cd6e39ac57cb1d33a5a83281e
Oliver Alt <oa@lacheweg21.de>
2023-07-05 14:05:57 +0200

    Initial code commit

---
+++ src/MDD4All.SpecIF.ViewModels.Testing/TestResourceNodeViewModel.cs
@@ -0,0 +1,64 @@
+﻿using MDD4All.SpecIF.DataModels;
+using MDD4All.SpecIF.DataProvider.Contracts;
+using MDD4All.UI.DataModels.Tree;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MDD4All.SpecIF.ViewModels.Testing
+{
+    public class TestResourceNodeViewModel : ITreeNode
+    {
+
+        public TestResourceNodeViewModel(ISpecIfMetadataReader metadataReader,
+                                         ISpecIfDataReader dataReader,
+                                         ITree tree,
+                                         Node node)
+        {
+            _metadataReader = metadataReader;
+            _specIfDataReader = dataReader;
+            Tree = tree;
+            _node = node;
+        }
+
+        private ISpecIfMetadataReader _metadataReader;
+
+        public ISpecIfMetadataReader MetadataReader
+        {
+            get { return _metadataReader; }
+
+        }
+
+        private ISpecIfDataReader _specIfDataReader;
+
+        public ISpecIfDataReader DataReader
+        {
+            get
+            {
+                return _specIfDataReader;
+            }
+        }
+
+        private Node _node;
+
+        public ITree Tree { get; }
+
+        public ObservableCollection<ITreeNode> Children => throw new NotImplementedException();
+
+        public ITreeNode Parent { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Index => throw new NotImplementedException();
+
+        public bool HasChildNodes => throw new NotImplementedException();
+
+        public bool IsExpanded { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsSelected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool IsLoading => throw new NotImplementedException();
+
+        public bool IsDisabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event EventHandler TreeStateChanged;
+    }
+}
---
+++ src/MDD4All.SpecIF.ViewModels.Testing/TestSuiteSelectionTreeViewModel.cs
@@ -0,0 +1,87 @@
+﻿using MDD4All.SpecIF.DataModels;
+using MDD4All.SpecIF.DataProvider.Contracts;
+using MDD4All.UI.DataModels.Tree;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MDD4All.SpecIF.ViewModels.Testing
+{
+    public class TestSuiteSelectionTreeViewModel : ITree
+    {
+
+        public TestSuiteSelectionTreeViewModel(ISpecIfDataProviderFactory specIfDataProviderFactory,
+                                               Key key) 
+        {
+            _specIfDataProviderFactory = specIfDataProviderFactory;
+            _metadataReader = _specIfDataProviderFactory.MetadataReader;
+            _specIfDataWriter = _specIfDataProviderFactory.DataWriter;
+            _specIfDataReader = _specIfDataProviderFactory.DataReader;
+
+            Node hierarchyRoot = _specIfDataReader.GetHierarchyByKey(key);
+           
+            RootNode = InitializeTestSuteSelectionTree(hierarchyRoot);
+
+            _treeRootNodes = new ObservableCollection<ITreeNode>();
+            _treeRootNodes.Add(RootNode);
+
+            SelectedNode = RootNode;
+        }
+
+        private TestResourceNodeViewModel InitializeTestSuteSelectionTree(Node hierarchyRootNode)
+        {
+            TestResourceNodeViewModel result = null;
+            
+            // hier den Baum aufbauen!!
+
+
+            return result;
+        }
+
+        private ISpecIfDataProviderFactory _specIfDataProviderFactory;
+
+        private ISpecIfMetadataReader _metadataReader;
+
+        public ISpecIfMetadataReader MetadataReader
+        {
+            get { return _metadataReader; }
+
+        }
+
+        private ISpecIfDataReader _specIfDataReader;
+
+        public ISpecIfDataReader DataReader
+        {
+            get
+            {
+                return _specIfDataReader;
+            }
+        }
+
+        private ISpecIfDataWriter _specIfDataWriter;
+
+        public ISpecIfDataWriter DataWriter
+        {
+            get
+            {
+                return _specIfDataWriter;
+            }
+        }
+
+        public TestResourceNodeViewModel RootNode { get; set; }
+
+        private ObservableCollection<ITreeNode> _treeRootNodes = new ObservableCollection<ITreeNode>();
+
+
+        public ObservableCollection<ITreeNode> TreeRootNodes
+        {
+            get
+            {
+                return _treeRootNodes;
+            }
+        }
+
+        public ITreeNode SelectedNode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
